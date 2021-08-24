bbb20fad32b3d0efada7b45739af561a66e99464
kuleshdeshmukh <kuleshdeshmukh@gmail.com>
2021-08-24 19:49:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "oss" {
+    bucket              = "jci-state-bucket"
+    prefix              = "env:"
+    key                 = "terraform.tfstate"
+    acl                 = "private"
+    region              = "ap-south-1"
+    encrypt             = "false"
+      }
+}
+
 provider "alicloud" {
   access_key = var.access_key
   secret_key = var.secret_key
